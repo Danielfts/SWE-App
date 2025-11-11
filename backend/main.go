6ed120897e55727c4509e583829ce24ae8d1ae4e
Danielfts <d.trivino@uniandes.edu.co>
2025-11-11 17:50:48 -0500

    Feature: Get data from db successfully

--- backend/main.go
+++ backend/main.go
@@ -2,10 +2,63 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"stocks/domain"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+func initDb() *gorm.DB {
+	if err := godotenv.Load(); err != nil {
+		log.Println("Did not find .env file")
+	} else {
+		log.Println("Found .env file!")
+	}
+
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error connecting to db: %v", err)
+	}
+	log.Println("Db connection successfull")
+
+	sqlDb, err := db.DB()
+	if err != nil {
+		log.Fatalf("Error getting db object")
+	}
+	if err := sqlDb.Ping(); err != nil {
+		log.Fatalf("Db not reachable %v", err)
+	}
+	log.Println("Db reachable")
+	return db
+}
+
+func getFirstStocks(db *gorm.DB) ([]domain.Stock, error) {
+	var stocks []domain.Stock
+	result := db.Limit(5).Find(&stocks)
+	return stocks, result.Error
+}
+
 func main() {
+	db := initDb()
+	stocks, err := getFirstStocks(db)
+	if err != nil {
+		log.Fatalf("Error getting stocks %v", err)
+	} else {
+		fmt.Printf("First 5 stocks: %v\n", stocks)
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
